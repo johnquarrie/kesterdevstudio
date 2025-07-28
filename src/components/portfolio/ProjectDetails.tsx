a711444587fc586f0351e78ebca6f9e8d0737117
Opia12345 <preciousopia7@gmail.com>
2025-07-28 11:16:21 +0100

    chore: updated the api endpoint

--- src/components/portfolio/ProjectDetails.tsx
+++ src/components/portfolio/ProjectDetails.tsx
@@ -26,7 +26,7 @@ export default function ProjectDetails() {
     const fetchProject = async () => {
       try {
         const res = await axios.get<{ data: Project }>(
-          `https://file-uploads-server.onrender.com/uploads/${id}`
+          `https://shimmering-curiosity-production.up.railway.app/${id}`
         );
         setProject(res.data.data);
       } catch (err: unknown) {
