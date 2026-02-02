fa458ac0fc15e7e49d8ce15fd6dead8ab61f8c75
KesterJosh <adeyelukester2@gmail.com>
2026-02-02 08:40:50 +0100

    Proper SEO

--- kesterstudio/app/layout.tsx
+++ kesterstudio/app/layout.tsx
@@ -51,18 +51,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>Kester Dev Studio</title>
+        <meta
+          name="description"
+          content="Kester Dev Studio is a software development studio that specializes in building custom software solutions for businesses."
+        />
+
         <meta property="og:title" content="Kester Dev Studio" />
         <meta
           property="og:description"
           content="Kester Dev Studio is a software development studio that specializes in building custom software solutions for businesses."
         />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.kesterdevstudio.com/" />
+        <meta property="og:site_name" content="Kester Dev Studio" />
         <meta property="og:image" content="https://www.kesterdevstudio.com/kesterdev.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@Kesterdevstudio" />
+        <meta name="twitter:title" content="Kester Dev Studio" />
+        <meta
+          name="twitter:description"
+          content="Kester Dev Studio is a software development studio that specializes in building custom software solutions for businesses."
+        />
         <meta name="twitter:image" content="https://www.kesterdevstudio.com/kesterdev.png" />
       </head>
+
       <body className={`${inter.variable} antialiased`}>
         <NextTopLoader color="#6C0BDB" showSpinner={false} />
         <main className="flex flex-col w-full relative bg-black">
